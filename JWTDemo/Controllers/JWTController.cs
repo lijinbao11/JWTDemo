166727c91f2922d6593a025bb862eb059b3da92d
lijinbao11 <l18994791837@163.com>
2020-09-19 11:42:11 +0800

    这是一个控制器

---
+++ JWTDemo/Controllers/JWTController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTDemo.Controllers
+{
+    public class JWTController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
